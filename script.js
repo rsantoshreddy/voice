aaba5cc1d3a6ed1c3b186e72ed3b21e302b0ce97
R Santosh Reddy <rsantoshreddy@users.noreply.github.com>
2019-07-25 00:11:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function startDictation() {
+  window.event.preventDefault();
+  if (window.hasOwnProperty('webkitSpeechRecognition')) {
+    var recognition = new webkitSpeechRecognition();
+
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.lang = 'en-US';
+    recognition.start();
+
+    recognition.onresult = function(e) {
+      document.getElementById('transcript').value = e.results[0][0].transcript;
+      recognition.stop();
+    };
+
+    recognition.onerror = function(e) {
+      recognition.stop();
+    };
+  }
+}
